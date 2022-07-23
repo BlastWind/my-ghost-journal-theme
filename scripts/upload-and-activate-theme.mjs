b8bdefa2784ddab401c379b6c8119577223efcbd
blastwind <andrewchen14250@gmail.com>
2022-07-22 20:11:54 -0400

    debug

--- scripts/upload-and-activate-theme.mjs
+++ scripts/upload-and-activate-theme.mjs
@@ -1,10 +1,10 @@
 import GhostAdminAPI from "@tryghost/admin-api";
+console.log(process.env);
 const api = new GhostAdminAPI({
   url: process.env.URL,
   key: process.env.ADMIN_KEY,
   version: "v5.0",
 });
-console.log(process.env);
 const data = { file: "../dist/journal.zip" };
 // api.themes.upload returns theme object, which we extract the name of to invoke activate
 // Unfortunately, @tryghost/admin-api doesn't have TS support yet
